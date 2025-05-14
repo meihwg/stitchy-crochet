b82696c4f766c84200994587700bc88ae88be497
Laureen Belgrand <belgrandlaureen.tg2@gmail.com>
2025-05-14 17:40:23 +0200

    add lang icon

--- src/components/pages/about/about.tsx
+++ src/components/pages/about/about.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     return (
         <div className="about">
             <div className="about-container">
-                <h2><div className="icon-uk"></div>About</h2>
+                <h2>About</h2>
                 <p>... Coming soon !</p>
             </div>
         </div>
--- src/components/pages/settings/settings.tsx
+++ src/components/pages/settings/settings.tsx
@@ -5,7 +5,7 @@ const Settings: React.FC = () => {
     return (
         <div className="settings">
             <div className="settings-container">
-                <h2><div className="icon-uk"></div>Settings</h2>
+                <h2>Settings</h2>
                 <p>... Coming soon !</p>
             </div>
         </div>
